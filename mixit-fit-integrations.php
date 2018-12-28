1a16fdd732a0c3959623191d593a21ab597801c2
Dan D <dan.dulaney07@gmail.com>
2018-12-27 22:00:16 -0600

    Added check for GF being installed

--- mixit-fit-integrations.php
+++ mixit-fit-integrations.php
@@ -15,22 +15,25 @@ License URI:
  **********************************************************/
 function mixit_setup_api_functions() {
 
-    //Functions specific to GF: dynamic population
-    require_once( plugin_dir_path( __FILE__ ) . 'gf/gf-dynamic-population.php');
+    if(class_exists( 'GFForms' )) {
 
-    //Functions specific to GF: custom validation
-    require_once( plugin_dir_path( __FILE__ ) . 'gf/gf-custom-validations.php');
+        //Functions specific to GF: dynamic population
+        require_once( plugin_dir_path( __FILE__ ) . 'gf/gf-dynamic-population.php');
 
-    //Functions specific to GF: post submit handling
-    require_once( plugin_dir_path( __FILE__ ) . 'gf/gf-post-submit.php');
+        //Functions specific to GF: custom validation
+        require_once( plugin_dir_path( __FILE__ ) . 'gf/gf-custom-validations.php');
 
-    //Functions specific to GF: Auto-delete entries
-    require_once( plugin_dir_path( __FILE__ ) . 'gf/gf-autodelete-entries.php');
+        //Functions specific to GF: post submit handling
+        require_once( plugin_dir_path( __FILE__ ) . 'gf/gf-post-submit.php');
 
-    //Functions specific to Fitbit
-    require_once( plugin_dir_path( __FILE__ ) . 'integrations/fitbit_api.php');
+        //Functions specific to GF: Auto-delete entries
+        require_once( plugin_dir_path( __FILE__ ) . 'gf/gf-autodelete-entries.php');
 
-    //Functions specitic to Habitica
-    require_once( plugin_dir_path( __FILE__ ) . 'integrations/habitica_api.php');
+        //Functions specific to Fitbit
+        require_once( plugin_dir_path( __FILE__ ) . 'integrations/fitbit_api.php');
+
+        //Functions specitic to Habitica
+        require_once( plugin_dir_path( __FILE__ ) . 'integrations/habitica_api.php');
+    }
 }
 add_action( 'plugins_loaded', 'mixit_setup_api_functions' );
